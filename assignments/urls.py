010cf4010904189f68617e92614e33f5b225907a
Unnati <rajunnati6@gmail.com>
2026-06-18 13:45:05 +0530

    Add assignment detail page

--- assignments/urls.py
+++ assignments/urls.py
@@ -4,5 +4,5 @@ from .import views
 urlpatterns = [
     path('', views.assignment_list, name='assignment_list'),
     path('create/', views.assignment_create, name='assignment_create'),
-    path('<int:pk>/', views.assignment_detail, name='assignment_detail'),
+    path('assignment/<int:pk>/', views.assignment_detail, name='assignment_detail'),
 ]
